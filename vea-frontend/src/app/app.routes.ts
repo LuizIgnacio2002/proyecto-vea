49a3e5ccdef9bc8a73c307d9e39e7fe3c9cb5a83
anycodef <anycodefast1@gmail.com>
2024-05-04 02:52:47 -0500

    Añadir rutas

--- vea-frontend/src/app/app.routes.ts
+++ vea-frontend/src/app/app.routes.ts
@@ -1,3 +1,39 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DetallesCarritoComponent } from './detalles-carrito/detalles-carrito.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
+import { HistorialComponent } from './historial/historial.component';
+import { AnaliticasComponent } from './analiticas/analiticas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: "VEA"
+    },
+    {
+        path:'carrito/:id',
+        component: DetallesCarritoComponent,
+        title: "Detalles Carrito"
+    },
+    {
+        path:'carrito/:id',
+        component: DetallesCarritoComponent,
+        title: "Detalles Carrito"
+    },  
+    {
+        path:'configuracion',
+        component: ConfiguracionComponent,
+        title: "Configuración"
+    },
+    {
+        path: 'historial',
+        component: HistorialComponent,
+        title: "Historial"
+    },
+    {
+        path: 'analiticas',
+        component: AnaliticasComponent,
+        title: "Analiticas"
+    }
+];
