36196d8e255727cff1786a55e80bf0114f513121
Jorge Ktch <jorgektch@gmail.com>
2024-05-12 11:10:27 -0500

    Separar archivos hpp y cpp

--- vea-estructuras/ArbolBB.hpp
+++ vea-estructuras/ArbolBB.hpp
@@ -1,13 +1,18 @@
+#ifndef ARBOLBB_H
+#define ARBOLBB_H
+
 #include <functional>
 
 using namespace std;
 
 template <class T>
 class NodoArbolBB{
-    
+
 };
 
 template <class T>
 class ArbolBB{
     
 };
+
+#endif
