517ac895817864b8ca2d9c1e79a88acfe0e5751b
Jorge Ktch <jorgektch@gmail.com>
2024-05-12 00:06:34 -0500

    Definir funciones lambda

--- vea-estructuras/Aplicacion.hpp
+++ vea-estructuras/Aplicacion.hpp
@@ -4,6 +4,9 @@
 #include "TablaHashC.hpp"
 
 #include "Registro.hpp"
+#include <functional>
+
+using namespace std;
 
 class Aplicacion{
 private:
@@ -11,13 +14,34 @@ private:
     ArbolAVL<Registro> arbolavl;
     TablaHashA<Registro> tablahasha;
     TablaHashC<Registro> tablahashc;
+    function<int(T, T)> comparacion;
 public:
     Aplicacion(){
+        auto comparacionCodigo = [](Registro r1, Registro r2){
+            if(r1.getCodigo() == r2.getCodigo()){
+                return 0;
+            }else if(r1.getCodigo() < r2.getCodigo()){
+                return 1;
+            }else{
+                return -1;
+            }
+        };
 
+        auto comparacionSueldo = [](Registro r1, Registro r2){
+            if(r1.getSueldo() == r2.getSueldo()){
+                return 0;
+            }else if(r1.getSueldo() < r2.getSueldo()){
+                return 1;
+            }else{
+                return -1;
+            }
+        };
+
+        this->comparacion = comparacionCodigo;
     }
     ~Aplicacion(){}
-    
-    void iniciar(){
 
+    void iniciar(){
+        
     }
 };
