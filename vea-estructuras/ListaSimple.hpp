eebbd1d5237fdfbe7029a414028178eecd99c119
Jorge Ktch <jorgektch@gmail.com>
2024-05-12 11:16:21 -0500

    Separar archivos hpp y cpp

---
+++ vea-estructuras/ListaSimple.hpp
@@ -0,0 +1,18 @@
+#ifndef LISTASIMPLE_H
+#define LISTASIMPLE_H
+
+#include <functional>
+
+using namespace std;
+
+template <class T>
+class NodoListaSimple{
+
+};
+
+template <class T>
+class ListaSimple{
+    
+};
+
+#endif
